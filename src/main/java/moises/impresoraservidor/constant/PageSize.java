9357fb8714a292c2868b2c431893a4f6a2f59c0f
Moises <alejandropea12345gmail.com>
2025-06-06 17:48:18 -0500

    Constantes

---
+++ src/main/java/moises/impresoraservidor/constant/PageSize.java
@@ -0,0 +1,8 @@
+package moises.impresoraservidor.constant;
+
+public class PageSize {
+
+    public static final float A4_WIDTH = 595.27563f;
+    public static final float A4_HEIGHT = 841.8898f;
+
+}
